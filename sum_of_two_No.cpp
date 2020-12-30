a10b1557c617e830103af41964e47cd28c3e39de
Richa Dwivedi <richa4897dwivedi@gmail.com>
2020-12-30 13:01:50 +0530

    C++ Program to Print Sum of Two Numbers.

---
+++ sum_of_two_No.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cout<<"Enter First Number\n";
+    cin>>a;
+
+    cout<<"Enter Second Number\n";
+    cin>>b;
+
+    cout<<"Sum of two Numbers is\n"<<a+b;
+
+    return 0;
+}
